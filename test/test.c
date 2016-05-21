65083617cdc180b8845c495657c6ce9900b05a47
Бобров Александр Александрович <ip511s04@awp05.csc.local>
2016-05-21 11:04:36 +0600

    asdf

--- test/test.c
+++ test/test.c
@@ -9,11 +9,27 @@ CTEST(urav_suite, test__equation)
 
  	urav (a, b, c, &x1, &x2);
  
-	const float expected_x1 = -2;
-	const float expected_x2 = -2;
+	 float expected_x1 = -2;
+	 float expected_x2 = -2;
 	
 	ASSERT_DBL_NEAR(expected_x1, x1);
 	ASSERT_DBL_NEAR(expected_x2, x2);
 }
+
+CTEST1(urav_suite, test__equation2) 
+{
+	const int a =1, b = 6, c = 9;
+
+        float x1, x2;
+
+ 	urav (a, b, c, &x1, &x2);
+ 
+	 float expected_x1 = -3;
+	 float expected_x2 = -3;
+	
+	ASSERT_DBL_NEAR(expected_x1, x1);
+	ASSERT_DBL_NEAR(expected_x2, x2);
+}
+
 
 
